d67ec9921f685f0b3377ce36487567024a067493
crist138 <cristoballongares@gmail.com>
2022-11-06 11:04:26 -0600

    Lo muestra bien

--- tablero/Gato.java
+++ tablero/Gato.java
@@ -6,7 +6,7 @@ public class Gato{
 
 
 //Atributos
-	int[][] tablero = new int[3][3];
+	String[][] tablero = {{".",".","."},{".",".","."},{".",".","."}};
 
 
 //Metodos
@@ -14,22 +14,30 @@ public class Gato{
 		int i,j;
 
 
-
+		/* Test para llenarlo por interaccion
 		for (i=0;i<3;i++){
 			for(j=0;j<3;j++){
 				System.out.print("Ingresa el dato para la posicion ["+i+"] ["+j+"]\n: ");
-				tablero[i][j] = in.nextInt();
+				tablero[i][j] = in.nextLine();
 			}
 		}
 
-		for (i=0;i<3;i++){
-			for(j=0;j<3;j++){
-				if(j>0 || j<3){
-					System.out.print(tablero[i][j]+"|");
-				} 
+		*/
 
+		/*System.out.print("\t\t---------------\n"); */
+		System.out.println("\n\n");
+		for (i=0;i<3;i++){
+			System.out.print("\t\t");
+			for(j=0;j<3;j++){
+				
+					System.out.print("| "+tablero[i][j]+" |");
+
+				
 				if(j==2){
-					System.out.println();
+					System.out.println("\n\n\n");
+
+				/*	System.out.print("\t\t---------------\n"); */
+
 				}
 			}
 		}
